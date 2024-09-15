45485b9902cd0e741903cece0334d40e80d84afc
RonanGreen1 <ronangreen2003@gmail.com>
2024-09-15 19:09:31 +0100

    added input and check for input

---
+++ Con_dev_Test_1/go.mod
@@ -0,0 +1,3 @@
+module Con_dev_Test_1
+
+go 1.23.1
--- Con_dev_Test_1/main.go
+++ Con_dev_Test_1/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func main() {
+
+	// This program i used for transfering roman numerials into base 10 decimal numbers
+
+	var roman_numerial string
+
+	fmt.Println("Enter Roman Numerials")
+	fmt.Scanln(&roman_numerial)
+
+	if len(roman_numerial) > 15 {
+		fmt.Println("Error: Input cannot be more than 15 characters.")
+		return
+	}
+
+	// Define allowed characters (Roman numerals: I, V, X, L, C, D, M),
+	// Return only boolean 'matched' then check if parameters were met
+	validInputPattern := "^[IVXLCDMivxlcdm]+$"
+	matched, _ := regexp.MatchString(validInputPattern, roman_numerial)
+
+	if !matched {
+		fmt.Println("Incorrect Input, please follow the following parameters:\nOnly use the following Roman numeral characters: I, V, X, L, C, D, M")
+	} else {
+		
+	}
+
+}

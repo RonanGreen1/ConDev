02f64e0ba4a49b575feb790af8b8e9adad56cc35
RonanGreen1 <ronangreen2003@gmail.com>
2024-09-30 11:28:38 +0100

    added readme

---
+++ Con_dev_Test_1/README.md
@@ -0,0 +1,3 @@
+	This program is used for transfering roman numerials into base 10 decimal numbers.
+	To run this code make sure to have go downloaded then just clone the repo and open the folder in the ternimal.
+	Then run go run main.go when you have cd into the folder.
--- Con_dev_Test_1/main.go
+++ Con_dev_Test_1/main.go
@@ -11,10 +11,6 @@ import (
 
 func main() {
 
-	// This program is used for transfering roman numerials into base 10 decimal numbers.
-	// To run this code make sure to have go downloaded then just clone the repo and open the folder in the ternimal.
-	// Then run go run main.go when you have cd into the folder.
-
 	var romanNumeral string //string of roman numerals input by user
 
 	convertion := map[string]int{

45c995e6c77eeb55694b85ce0b6939fa5eb19109
RonanGreen1 <ronangreen2003@gmail.com>
2024-11-04 12:02:37 +0000

    added comments

--- Producer Consumer/Pro_Con.go
+++ Producer Consumer/Pro_Con.go
@@ -16,25 +16,30 @@ import (
     "time"
 )
 
+// producer sends integers to the channel 'ch'.
+// It runs in a loop from 0 to 9, simulating a delay with time.Sleep.
+// After sending all values, it closes the channel.
 func producer(ch chan int) {
     for i := 0; i < 10; i++ {
-        time.Sleep(time.Second)
-        fmt.Println("Producer: sending", i)
-        ch <- i
+        time.Sleep(time.Second) // Simulate some work with a 1-second delay
+        fmt.Println("Producer: sending", i) // Log the value being sent
+        ch <- i // Send the value to the channel
     }
-    close(ch)
+    close(ch) // Close the channel to signal no more values will be sent
 }
 
+// consumer receives integers from the read-only channel 'ch'.
+// It processes values received from the channel until it is closed.
 func consumer(ch <-chan int) {
-    for i := range ch {
-        time.Sleep(time.Second)
-        fmt.Println("Consumer: receiving", i)
+    for i := range ch { // Read values from the channel until it's closed
+        time.Sleep(time.Second) // Simulate some work with a 1-second delay
+        fmt.Println("Consumer: receiving", i) // Log the value being received
     }
 }
 
 func main() {
-    ch := make(chan int)
-    go producer(ch)
-    go consumer(ch)
-    time.Sleep(time.Second * 10)
+    ch := make(chan int) // Create an unbuffered channel of type int
+    go producer(ch) // Start the producer goroutine
+    go consumer(ch) // Start the consumer goroutine
+    time.Sleep(time.Second * 10) // Allow some time for producer and consumer to complete their work
 }
